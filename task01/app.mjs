f23c424c44678fe8322e25b04e70d7bf4ab2345b
Fry98 <filip-toman@seznam.cz>
2022-12-02 15:53:01 +0100

    Initial commit

---
+++ task01/app.mjs
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('input.txt'),
+  crlfDelay: Infinity
+});
+
+const sums = [];
+let acc = 0;
+rl.on('line', (line) => {
+  if (line.length) {
+    acc += Number(line);
+  } else {
+    sums.push(acc);
+    acc = 0;
+  }
+});
+
+rl.on('close', () => {
+  sums.sort((a, b) => b - a);
+  console.log(sums[0] + sums[1] + sums[2]);
+});
