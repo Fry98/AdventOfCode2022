ee67a38fd329821393046da46a4ebe24182f5ab4
Fry98 <filip-toman@seznam.cz>
2022-12-03 18:27:26 +0100

    D3P2

--- day03/app.mjs
+++ day03/app.mjs
@@ -1,10 +1,11 @@
 import fs from 'fs';
 
-const table = new Set();
+const table = new Map();
 const data = fs.readFileSync('input.txt', 'utf8').split('\n').filter(x => x);
-
 let total = 0;
-for (const line of data) {
+
+// Part 1:
+/* for (const line of data) {
   table.clear();
   const comp1 = line.substring(0, line.length / 2);
   const comp2 = line.substring(line.length / 2);
@@ -18,12 +19,29 @@ for (const line of data) {
       break;
     }
   }
+} */
+
+// Part 2:
+for (let i = 0; i < data.length; i += 3) {
+  table.clear();
+  const line1 = data[i];
+  const line2 = data[i + 1];
+  const line3 = data[i + 2];
+
+  for (const ch of line1) table.set(ch, 1);
+  for (const ch of line2) table.get(ch) && table.set(ch, 2);
+  for (const ch of line3) {
+    if (table.get(ch) === 2) {
+      total += getValue(ch);
+      break;
+    }
+  }
 }
 
 console.log(total);
 
 /** @param {string} ch */
-function addToTotal(ch) {
+function getValue(ch) {
   const code = ch.charCodeAt(0);
-  total += code > 96 ? code - 96 : code - 38;
+  return code > 96 ? code - 96 : code - 38;
 }
