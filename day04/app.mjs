3653e2362cf1bf233a6d318b06a37c099665b663
Fry98 <filip-toman@seznam.cz>
2022-12-04 07:50:51 +0100

    Update D4 parsing

--- day04/app.mjs
+++ day04/app.mjs
@@ -4,9 +4,8 @@ let total = 0;
 const data = fs.readFileSync('input.txt', 'utf8').split('\n').filter(x => x);
 
 for (const line of data) {
-  const [r1, r2] = line.split(',');
-  const [r1Start, r1End] = parseRange(r1);
-  const [r2Start, r2End] = parseRange(r2);
+  const [r1Start, r1End, r2Start, r2End] = line.split(',')
+    .flatMap(x => x.split('-').map(x => Number(x)));
 
   // Part 1:
   /* if (
@@ -22,8 +21,3 @@ for (const line of data) {
 }
 
 console.log(total);
-
-function parseRange(inp) {
-  const [start, end] = inp.split('-');
-  return [Number(start), Number(end)];
-}
